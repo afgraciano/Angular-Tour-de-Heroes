402040f8d1f159f96ae6e8b6492c719f30503816
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2023-09-05 07:33:54 -0500

    4 commit mostrar una lista

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero'; //importo la interfaz Hero
+import { HEROES } from '../mock-heroes';
 
-@Component
+
+/*@Component
 ({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
-})
-export class HeroesComponent
+})*/
+
+@Component
+  ({
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
+  })
+/*export class HeroesComponent
 {//refactorizo la propiedad del componente hero para que sea tipo Hero. inicializo con un id de 1 y el nombre Windstorm
   hero: Hero =
   {
     id: 1,
     name: 'Windstorm'
   };
+}*/
+export class HeroesComponent {
+  //defina una propiedad de componente llamada heroespara exponer la HEROESmatriz para su vinculación
+  heroes = HEROES;
+  //Agregue el controlador de eventos de clic
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
