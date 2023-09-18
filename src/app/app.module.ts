7788b5e8727fb66617b0b1301aa525f15bd69c09
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2023-09-18 17:42:11 -0500

    8 commit obtener datos de un servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here importando modulo ngmodule
+import { HttpClientModule } from '@angular/common/http'; //agrego a raiz el httpclient module importandolo
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';//importo automaticamente HeroesComponent para delcararla automaticamente abajo
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
+
+
 
 @NgModule({
 
@@ -18,15 +29,23 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule//reingresamos AppRoutingModule para las rutas de ir a otra pagina
+    AppRoutingModule,//reingresamos AppRoutingModule para las rutas de ir a otra pagina
+    HttpClientModule, //para poner el httpclient a disposicion en todas partes
+
+      // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(  //El forRoot()método de configuración toma una InMemoryDataServiceclase que prepara la base de datos en memoria.
+      InMemoryDataService, { dataEncapsulation: false }//El in-memory-data.service.tsarchivo asume la función de mock-heroes.ts
+    )
   ],
   declarations: [
     AppComponent,
     DashboardComponent,//declaro automaticamente HeroesComponent
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
-
+    MessagesComponent,
+    HeroSearchComponent
   ],
   /*providers: [ // no need to place any providers due to the `providedIn` flag...
   ],*/
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -49,6 +49,22 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
+//para agregar otro nuevo heroe
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+//para eliminar un heroe
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
 }
 
 
